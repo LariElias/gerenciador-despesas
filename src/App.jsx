c05ea3035a871f1fd3e047b2625b6a956beabe12
LariElias <larissaelias4545@gmail.com>
2024-11-30 16:38:27 -0300

    atualizações de filtro, editar e cancelar

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className=" flex items-center justify-center bg-gray-100">
             {token ? (
                 <Dashboard token={token} />
             ) : (
